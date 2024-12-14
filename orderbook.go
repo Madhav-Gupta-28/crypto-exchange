d78a9a264832e5d10797df1ad166bd74aa3bf5ab
Madhav-Gupta-28 <guptamadhav2828@gmail.com>
2024-12-14 23:02:58 +0530

    completed episode 2 of course

--- orderbook.go
+++ orderbook.go
@@ -46,14 +46,14 @@ type Limit struct {
 
 type Limits []*Limit
 
-// For Asks
+// For ask
 type ByBestAsk struct{ Limits }
 
 func (a ByBestAsk) Len() int           { return len(a.Limits) }
 func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
 func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
 
-// For Bids
+// For bids
 type ByBestBid struct{ Limits }
 
 func (b ByBestBid) Len() int           { return len(b.Limits) }
@@ -96,9 +96,78 @@ func (l *Limit) DeleteOrder(o *Order) string {
 	return "Order not found"
 }
 
+func (l *Limit) fillOrder(a *Order, b *Order) Match {
+
+	var (
+		bid        *Order
+		ask        *Order
+		SizeFilled float64
+	)
+
+	if a.Bid {
+		bid = a
+		ask = b
+	} else {
+		bid = b
+		ask = a
+	}
+
+	if a.size >= b.size {
+		a.size -= b.size
+		SizeFilled = b.size
+		b.size = 0
+	} else {
+		b.size -= a.size
+		SizeFilled = a.size
+		a.size = 0
+	}
+
+	return Match{bid, ask, SizeFilled, l.Price}
+}
+
+func (o *Order) isFilled() bool {
+
+	if o.size == 0.0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (l *Limit) Fill(o *Order) []Match {
+
+	var (
+		matches        []Match
+		ordersToDelete []*Order
+	)
+
+	for _, order := range l.Orders {
+		match := l.fillOrder(order, o)
+		matches = append(matches, match)
+
+		l.TotalVolume -= match.SizeFilled
+
+		if order.isFilled() {
+			ordersToDelete = append(ordersToDelete, order)
+		}
+
+		if order.isFilled() {
+			break
+		}
+
+	}
+
+	for _, order := range ordersToDelete {
+		l.DeleteOrder(order)
+	}
+
+	return matches
+
+}
+
 type Orderbook struct {
-	Asks []*Limit
-	Bids []*Limit
+	ask  []*Limit
+	bids []*Limit
 
 	AskLimits map[float64]*Limit
 	BidLimits map[float64]*Limit
@@ -106,26 +175,55 @@ type Orderbook struct {
 
 func NewOrderbook() *Orderbook {
 	return &Orderbook{
-		Asks:      []*Limit{},
-		Bids:      []*Limit{},
+		ask:       []*Limit{},
+		bids:      []*Limit{},
 		AskLimits: make(map[float64]*Limit),
 		BidLimits: make(map[float64]*Limit),
 	}
 }
 
-func (ob *Orderbook) PlaceOrder(price float64, o *Order) []Match {
-	// try to match the order - matching logic (market orders)
+func (ob *Orderbook) PlaceMarketOrder(o *Order) []Match {
 
-	// add the rest of order to the books
+	matches := []Match{}
 
-	if o.size > 0.0 {
-		ob.Add(price, o)
+	if o.Bid {
+
+		if ob.AskTotalVolumne() < o.size {
+			panic(("not enough volume to fill order in the orderbook"))
+		}
+
+		for _, limit := range ob.Asks() {
+			limitmatches := limit.Fill(o)
+			matches = append(matches, limitmatches...)
+
+			if len(limit.Orders) == 0 {
+				ob.clearLimit(true, limit)
+			}
+
+		}
+
+	} else {
+
+		if ob.BidTotalVolumne() < o.size {
+			panic(("not enough volume to fill order in the orderbook"))
+		}
+
+		for _, limit := range ob.Bids() {
+			limitmatches := limit.Fill(o)
+			matches = append(matches, limitmatches...)
+
+			if len(limit.Orders) == 0 {
+				ob.clearLimit(false, limit)
+			}
+
+		}
 	}
 
-	return []Match{}
+	return matches
+
 }
 
-func (ob *Orderbook) Add(price float64, o *Order) {
+func (ob *Orderbook) PlaceLimitOrder(price float64, o *Order) {
 
 	var limit *Limit
 
@@ -138,15 +236,82 @@ func (ob *Orderbook) Add(price float64, o *Order) {
 
 	if limit == nil {
 		limit = NewLimit(price)
-		limit.AddOrder(o)
+		// limit.AddOrder(o)
 
 		if o.Bid {
-			ob.Bids = append(ob.Asks, limit)
+			ob.bids = append(ob.bids, limit)
 			ob.BidLimits[price] = limit
 		} else {
-			ob.Asks = append(ob.Asks, limit)
+			ob.ask = append(ob.ask, limit)
 			ob.AskLimits[price] = limit
 		}
 	}
+
+	limit.AddOrder(o)
+
+}
+
+func (ob *Orderbook) cancelOrder(o *Order) {
+	limit := o.Limit
+	limit.DeleteOrder(o)
+}
+
+func (ob *Orderbook) clearLimit(bid bool, l *Limit) {
+	if bid {
+		delete(ob.BidLimits, l.Price)
+		for i := 0; i < len(ob.bids); i++ {
+			if ob.bids[i] == l {
+				ob.bids[i] = ob.bids[len(ob.bids)-1]
+				ob.bids = ob.bids[:len(ob.bids)-1]
+			}
+		}
+	} else {
+		delete(ob.AskLimits, l.Price)
+		for i := 0; i < len(ob.ask); i++ {
+			if ob.ask[i] == l {
+				ob.ask[i] = ob.ask[len(ob.ask)-1]
+				ob.ask = ob.ask[:len(ob.ask)-1]
+			}
+		}
+
+	}
+
+}
+
+func (ob *Orderbook) BidTotalVolumne() float64 {
+	totalVolumne := 0.0
+
+	for i := 0; i < len(ob.bids); i++ {
+
+		totalVolumne += ob.bids[i].TotalVolume
+
+	}
+	return totalVolumne
+
+}
+
+func (ob *Orderbook) AskTotalVolumne() float64 {
+	totalVolumne := 0.0
+
+	for i := 0; i < len(ob.ask); i++ {
+
+		totalVolumne += ob.ask[i].TotalVolume
+
+	}
+	return totalVolumne
+
+}
+
+func (ob *Orderbook) Asks() []*Limit {
+
+	sort.Sort(ByBestAsk{ob.ask})
+	return ob.ask
+
+}
+
+func (ob *Orderbook) Bids() []*Limit {
+
+	sort.Sort(ByBestBid{ob.bids})
+	return ob.bids
 
 }
--- orderbook_test.go
+++ orderbook_test.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
+
+func assert(t *testing.T, a, b any) {
+
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("%+v != %+v ", a, b)
+	}
+}
 
 func TestLimit(t *testing.T) {
 	l := NewLimit(10_000)
@@ -15,6 +23,8 @@ func TestLimit(t *testing.T) {
 
 	l.AddOrder(buyorder1)
 
+	assert(t, len(l.Orders), 2)
+
 	fmt.Println(l)
 
 	// l.DeleteOrder(buyorder)
@@ -23,13 +33,81 @@ func TestLimit(t *testing.T) {
 
 }
 
-func TestOrderBook(t *testing.T) {
+func TestPlaceLimitOrder(t *testing.T) {
+	ob := NewOrderbook()
 
-	ob1 := NewOrderbook()
+	sellorder := NewOrder(false, 10)
 
-	buyOrder := NewOrder(true, 10)
+	ob.PlaceLimitOrder(10, sellorder)
 
-	ob1.PlaceOrder(18_000, buyOrder)
+	assert(t, len(ob.ask), 1)
+}
 
-	fmt.Println(ob1.Bids)
+func TestPlaceMarketOrder(t *testing.T) {
+	ob := NewOrderbook()
+
+	sellorder := NewOrder(false, 20)
+	ob.PlaceLimitOrder(10000, sellorder)
+	buyorder := NewOrder(true, 10)
+
+	// ob.PlaceMarketOrder(sellorder)
+	// ob.PlaceMarketOrder(buyorder)
+
+	matches := ob.PlaceMarketOrder(buyorder)
+
+	assert(t, len(matches), 1)
+
+	assert(t, ob.AskTotalVolumne(), 10.0)
+
+	// assert(t, matches[0].Ask, sellorder)
+	// assert(t, matches[0].Bid, buyorder)
+
+	fmt.Printf("%+v\n", matches)
+
+	assert(t, len(ob.ask), 1)
+}
+
+func TestPlaceMarketOrderMultifill(t *testing.T) {
+	ob := NewOrderbook()
+
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 10)
+	buyOrderC := NewOrder(true, 15)
+	buyOrderD := NewOrder(true, 20)
+
+	ob.PlaceLimitOrder(1000, buyOrderA)
+	ob.PlaceLimitOrder(9000, buyOrderB)
+	ob.PlaceLimitOrder(11000, buyOrderC)
+	ob.PlaceLimitOrder(1000, buyOrderD)
+
+	assert(t, ob.BidTotalVolumne(), 50.0)
+	// assert(t, ob.AskTotalVolumne(), 0.0)
+
+	// sellOrder := NewOrder(false, 20)
+	// matches := ob.PlaceMarketOrder(sellOrder)
+
+	// fmt.Printf("%+v\n", matches)
+
+	// assert(t, len(matches), 3)
+	// assert(t, len(ob.bids), 3)
+
+	// assert(t, ob.AskTotalVolumne(), 10.0)
+
+}
+
+func TestCancelOrder(t *testing.T) {
+	ob := NewOrderbook()
+
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 10)
+
+	ob.PlaceLimitOrder(1000, buyOrderA)
+	ob.PlaceLimitOrder(9000, buyOrderB)
+
+	assert(t, ob.BidTotalVolumne(), 15.0)
+
+	ob.cancelOrder(buyOrderB)
+
+	assert(t, ob.BidTotalVolumne(), 5.0)
+
 }
